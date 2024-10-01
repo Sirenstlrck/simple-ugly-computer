d5baf8e240ddfd2460f879be93ad8d4064d93897
Sirenstlrck <meysaf204@gmail.com>
2024-10-01 21:20:03 +0700

    Tests formatted

--- include/myConsole.h
+++ include/myConsole.h
@@ -1,6 +1,4 @@
 #ifndef _MY_CONSOLE_H
 #define _MY_CONSOLE_H
 
-
-
 #endif // _MY_CONSOLE_H
--- tests/saveLoad.c
+++ tests/saveLoad.c
@@ -6,15 +6,15 @@
 */
 
 #include "mySimpleComputer.h"
-#include <errno.h>
 #include <assert.h>
+#include <errno.h>
 
 const char *filename = "save.bin";
 
 void fill()
 {
-    sc_memoryInit();
-    for (int i = 0, ie = MEMORY_SIZE; i < ie; ++i)
+	sc_memoryInit();
+	for (int i = 0, ie = MEMORY_SIZE; i < ie; ++i)
 	{
 		sc_memorySet(i, i);
 	}
@@ -22,45 +22,46 @@ void fill()
 
 void save()
 {
-    int saveRes = sc_memorySave(filename);
-    if(saveRes == -1)
-    {
-        fprintf(stderr, "Failed to save memory to file: %s\n", strerror(errno));
-        abort();
-    }
+	int saveRes = sc_memorySave(filename);
+	if (saveRes == -1)
+	{
+		fprintf(stderr, "Failed to save memory to file: %s\n", strerror(errno));
+		abort();
+	}
 }
 
 void load()
 {
-    sc_memoryInit();
-    int loadRes = sc_memoryLoad(filename);
-    if(loadRes == -1)
-    {
-        fprintf(stderr, "Failed to load memory from file: %s\n", strerror(errno));
-        abort();
-    }
+	sc_memoryInit();
+	int loadRes = sc_memoryLoad(filename);
+	if (loadRes == -1)
+	{
+		fprintf(stderr, "Failed to load memory from file: %s\n",
+				strerror(errno));
+		abort();
+	}
 }
 
 void validate()
 {
-    for (int i = 0, ie = MEMORY_SIZE; i < ie; ++i)
+	for (int i = 0, ie = MEMORY_SIZE; i < ie; ++i)
 	{
-        int value = 0;
+		int value = 0;
 		int getRes = sc_memoryGet(i, &value);
-        if(getRes == -1)
-        {
-            fprintf(stderr, "Failed to get value by %d address", i);
-            abort();
-        }
-        assert(value == i);
+		if (getRes == -1)
+		{
+			fprintf(stderr, "Failed to get value by %d address", i);
+			abort();
+		}
+		assert(value == i);
 	}
 }
 
 int main()
 {
-    fill();
-    save();
-    load();
-    validate();
-    return 0;
+	fill();
+	save();
+	load();
+	validate();
+	return 0;
 }
