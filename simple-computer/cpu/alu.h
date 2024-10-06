1144e2c696bbff9803964c7819d9ff9503978b71
Sirenstlrck <meysaf204@gmail.com>
2024-10-06 23:09:04 +0700

    staged

--- simple-computer/cpu/alu.h
+++ simple-computer/cpu/alu.h
@@ -1,10 +1,21 @@
 #ifndef SC_ARITHMETIC_LOGIC_UNIT_H
 #define SC_ARITHMETIC_LOGIC_UNIT_H
 
-#include "state.h"
 #include <assert.h>
 
-static void sc_alu_start(int op, int operand) {}
+#include "cpu/operation.h"
+#include "state.h"
+
+static void sc_alu_start(int op, int operand)
+{
+	// all of theese requires load from memory
+	if (IS_OP_IN_RANGE(ALU, op) || (op >= And && op <= Xor) ||
+		(op >= Chl && op <= Neg))
+	// TODO: ubercomplicated funcs that requires 2 loads
+	{
+		loadData(operand);
+	}
+}
 
 static void sc_alu_continue(int op, int operand) {}
 
